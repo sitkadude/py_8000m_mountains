22a41c06ba592c27ce7f8a2a57391ce30a9a3bea
Lukas Jorgensen <Lukas.r.jorgensen@gmail.com>
2020-07-02 00:07:36 -0400

    nested dictionary

--- 8000m_mountains.py
+++ 8000m_mountains.py
@@ -1,116 +1,120 @@
-everest = {
-        'elevation': 8_848, 
-        'range': 'himalaya',
-        'sub_range': 'mahalangur',
-        'lat' = 27.988056,
-        'lon'= 86.925278
+mountains = {
+        'everest': {
+            'elevation': 8_848, 
+            'range': 'himalaya',
+            'sub_range': 'mahalangur',
+            'lat' : 27.988056,
+            'lon': 86.925278
+            },
+
+        'k2': {
+            'elevation': 8_611,
+            'range': 'karakoram',
+            'sub_range': 'baltoro',
+            'lat': 35.881389,
+            'lon':76.513333
+            },
+
+        'kangchenjunga': {
+            'elevation': 8_586,
+            'range': 'himalaya',
+            'sub_range': 'mahalangur',
+            'lat': 27.703333,
+            'lon': 88.1475
+            },
+
+        'lhotse': {
+            'elevation': 8_516,
+            'range': 'himalaya',
+            'sub_range': 'mahalangur',
+            'lat': 27.961667,
+            'lon': 86.933056
+            },
+
+        'makalu': {
+            'elevation': 8_485,
+            'range': 'himalaya',
+            'sub_range': 'mahalangur',
+            'lat': 27.889722,
+            'lon': 87.088889
+            },
+
+        'cho oyu': {
+            'elevation': 8_188,
+            'range': 'himalaya',
+            'sub_range': 'mahalangur',
+            'lat': 28.094167,
+            'lon': 86.660833
+            },
+
+        'dhaulagiri 1': {
+            'elevation': 8_167,
+            'range': 'himalaya',
+            'sub_range': 'dhaulagiri',
+            'lat': 28.696667,
+            'lon': 83.493056
+            },
+
+        'manaslu': {
+            'elevation': 8_163,
+            'range': 'himalaya',
+            'sub_range': 'mahalangur',
+            'lat': 28.55,
+            'lon': 84.560833 
+            },
+
+        'nanga parbat': {
+            'elevation': 8_126,
+            'range': 'himalaya',
+            'sub_range': 'nanga parbat',
+            'lat': 35.237222,
+            'lon': 74.589167
+            },
+
+        'annapurna 1': {
+            'elevation': 8_091,
+            'range': 'himalaya',
+            'sub_range': 'annapurna',
+            'lat': 28.595556,
+            'lon': 83.820278
+            },
+
+        'gasherbrum 1': {
+            'elevation': 8_080,
+            'range': 'karakoram',
+            'sub_range': 'baltoro',
+            'lat': 35.724444,
+            'lon': 76.696389 
+            },
+
+        'broad peak': {
+            'elevation': 8_051,
+            'range': 'karakoram',
+            'sub_range': 'baltoro',
+            'lat': 35.810556,
+            'lon': 76.568333 
+            },
+
+        'gasherbrum 2': {
+            'elevation': 8_035,
+            'range': 'karakoram',
+            'sub_range': 'baltoro',
+            'lat': 35.758333,
+            'lon': 76.653333
+            },
+
+        'shishapangma': {
+            'elevation': 8_027,
+            'range': 'himalaya',
+            'sub_range': 'jugal',
+            'lat': 28.353333,
+            'lon': 85.778611
+            }
         }
 
-k2 = {
-        'elevation': 8_611,
-        'range': 'karakoram',
-        'sub_range'= 'baltoro',
-        'lat': 35.881389,
-        'lon':76.513333
-        }
-
-kangchenjunga = {
-        'elevation': 8_586,
-        'range': 'himalaya'
-        'sub_range': 'mahalangur',
-        'lat': 27.703333,
-        'lon': 88.1475
-        }
-
-lhotse = {
-        'elevation': 8_516,
-        'range': 'himalaya'
-        'sub_range': 'mahalangur',
-        'lat': 27.961667,
-        'lon': 86.933056
-        }
-
-makalu = {
-        'elevation': 8_485,
-        'range': 'himalaya'
-        'sub_range': 'mahalangur',
-        'lat': 27.889722,
-        'lon': 87.088889
-        }
-
-cho_oyu = {
-        'elevation': 8_188,
-        'range': 'himalaya'
-        'sub_range': 'mahalangur',
-        'lat': 28.094167,
-        'lon': 86.660833
-        }
-
-dhaulagiri_i = {
-        'elevation': 8_167,
-        'range': 'himalaya'
-        'sub_range': 'dhaulagiri',
-        'lat': 28.696667,
-        'lon': 83.493056
-        }
-
-manaslu = {
-        'elevation': 8_163,
-        'range': 'himalaya'
-        'sub_range': 'mahalangur',
-        'lat': 28.55,
-        'lon': 84.560833 
-        }
-
-nanga_parbat = {
-        'elevation': 8_126,
-        'range': 'himalaya'
-        'sub_range': 'nanga parbat',
-        'lat': 35.237222,
-        'lon': 74.589167
-        }
-
-annapurna_i = {
-        'elevation': 8_091,
-        'range': 'himalaya'
-        'sub_range': 'annapurna',
-        'lat': 28.595556,
-        'lon': 83.820278
-        }
-
-gasherbrum_i = {
-        'elevation': 8_080,
-        'range': 'karakoram'
-        'sub_range': 'baltoro',
-        'lat': 35.724444,
-        'lon': 76.696389 
-        }
-
-broad_peak = {
-        'elevation': 8_051,
-        'range': 'karakoram'
-        'sub_range': 'baltoro',
-        'lat': 35.810556,
-        'lon': 76.568333 
-        }
-
-gasherbrum_ii = {
-        'elevation': 8_035,
-        'range': 'karakoram'
-        'sub_range': 'baltoro',
-        'lat': 35.758333,
-        'lon': 76.653333
-        }
-
-shishapangma = {
-        'elevation': 8_027,
-        'range': 'himalaya'
-        'sub_range': 'jugal',
-        'lat': 28.353333,
-        'lon': 85.778611
-        }
-
-
-8000_peaks = {}
-
-
+for mountain, mountain_info in mountains.items():
+    print(f'\nMountain: {mountain.title()}')
+    print(f"Elevation: {mountain_info['elevation']}")
+    print(f"Range: {mountain_info['sub_range'].title()} {mountain_info['range'].title()}")
+    print(f"Latitude: {mountain_info['lat']}")
+    print(f"Longitude: {mountain_info['lon']}")
